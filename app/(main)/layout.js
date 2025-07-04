7e1c9a1dc3c2fa1eaf35c5ec11ccfc858e2aa066
Aaliya3032 <khan.alia3032@gmail.com>
2025-07-04 12:10:20 +0530

    FH commit

---
+++ app/(main)/layout.js
@@ -0,0 +1,33 @@
+import Header from "@/components/Header";
+import "../globals.css";
+import Sidenav from "@/components/Sidenav";
+import Footer from "@/components/Footer";
+import Providers from "@/components/Providers";
+import { getUserByEmail } from "@/queries/users";
+import { auth } from "@/auth";
+
+export const metadata = {
+  title: "Blustock Consultants",
+  description: "A stock market academy",
+};
+
+export default async function MainLayout({ children }) {
+  const session = await auth();
+  let loggedInUser = null;
+  if (session) {
+    loggedInUser = await getUserByEmail(session?.user?.email);
+  }
+
+  return (
+    <Providers>
+      <Header loggedInUser={loggedInUser || ""} />
+      <main className="relative">
+        <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-40">
+          <Sidenav />
+        </div>
+        {children}
+      </main>
+      <Footer />
+    </Providers>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,17 @@
-import Header from "@/components/Header";
-import "./globals.css";
-import Sidenav from "@/components/Sidenav";
-import Footer from "@/components/Footer";
-import { connectDb } from "@/helper/db";
 import { cn } from "@/lib/utils";
-import Providers from "@/components/Providers";
 import { Toaster } from "sonner";
-import { getUserByEmail } from "@/queries/users";
-import { auth } from "@/auth";
-
-
-export const metadata = {
-  title: "Blustock Consultants",
-  description: "A stock market academy",
-};
+import "./globals.css";
+import { connectDb } from "@/helper/db";
 
 export default async function RootLayout({ children }) {
- await connectDb()
- const session = await auth()
- let loggedInUser = null;
- if (session) {
-  loggedInUser = await getUserByEmail(session?.user?.email);
-}
-  
-  return  (
+    const conn = await connectDb();
+    console.log("database connected",conn)
+  return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn("w-full")}>
-        <Providers>
-          <Header loggedInUser={loggedInUser || ''}/>
-          
-          <main
-          className="relative"
-          >
-            <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-40">
-            <Sidenav/>
-            </div>
-            {children}
-            <Toaster richColors position="top-center" />
-          </main>
-          <Footer/>
-          </Providers>
+        {children}
+
+        <Toaster richColors position="top-center" />
       </body>
     </html>
   );
