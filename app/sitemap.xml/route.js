2d1dce0d68c765303d5e256d87a7c70dd73bc50e
Aaliya3032 <khan.alia3032@gmail.com>
2025-07-08 12:23:42 +0530

    xml

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,35 @@
+// app/sitemap.xml/route.js
+export async function GET() {
+  const baseUrl = 'https://www.blustockconsultants.com'; 
+
+  const routes = [
+    '',            
+    '/about',       
+    '/contact',   
+    '/courses',   
+    '/contact',  
+    '/login',
+    '/register',
+    '/account',
+    '/dashboard'
+  ];
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${routes
+      .map(
+        (route) => `
+      <url>
+        <loc>${baseUrl}${route}</loc>
+      </url>
+    `
+      )
+      .join('')}
+  </urlset>`;
+
+  return new Response(body, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+}
