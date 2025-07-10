d6307e84ff3591559c1067b813c51d846410338d
Aaliya3032 <khan.alia3032@gmail.com>
2025-07-10 11:22:54 +0530

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,31 @@ const nextConfig = {
           hostname: "images.pexels.com"
       }
   ]
-  }  
+  }  ,
+   async redirects() {
+    return [
+        // 1. HTTP → HTTPS
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "blustockconsultants.com" }],
+        destination: "https://www.blustockconsultants.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "http://blustockconsultants.com" }],
+        destination: "https://www.blustockconsultants.com/:path*",
+        permanent: true,
+      },
+      // 2. non‑www → www
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "https://blustockconsultants.com" }],
+        destination: "https://www.blustockconsultants.com/:path*",
+        permanent: true,
+      },
+    ]
+   }
 };
 
 export default nextConfig;
