a0f2ac5dab126841f75c1d37ec3da23dd6d07a39
Aaliya3032 <khan.alia3032@gmail.com>
2025-08-10 16:03:43 +0530

    keywords for SEO

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,25 @@ import { connectDb } from "@/helper/db";
 export const metadata = {
   title: "Blustock Consultants - Best Stock Market Academy in Jaipur",
   description: "A stock market academy",
+   keywords: [
+    "Stock Market Academy Jaipur",
+    "Stock Market Academy Jothwara",
+    "Institutes for Stock Market",
+    "Trading Centres",
+    "Option Trading Services",
+    "Derivative Trading Services",
+    "Share Trading Institutes",
+    "Learn Stock Trading Jaipur",
+    "Best Stock Market Classes",
+    "Stock Market Courses in Jaipur",
+    "Share Market Training Jaipur",
+    "Investing Classes Jaipur",
+    "Blustock Consultants",
+    "Blustock",
+    "Stock Market Institute Jaipur",
+    "Trading Courses in Jaipur",
+    "Jaipur Stock Trading Academy"
+  ],
    icons: {
     icon: "/favicon.ico",
   },
