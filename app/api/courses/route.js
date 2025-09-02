5aa693fc6cbc6ec85a432a2e91c63daacad8dc85
Aaliya3032 <khan.alia3032@gmail.com>
2025-09-02 11:55:06 +0530

    headers but 6

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 import { NextResponse } from "next/server";
 import { getCourseList } from "@/queries/courses";
 
@@ -6,15 +9,15 @@ export async function GET() {
     const courses = await getCourseList();
 
     return NextResponse.json(
-      { success: true, data: courses || [] }, // always valid JSON
-      { status: 200 }
+      { success: true, data: courses || [] },
+      { status: 200 , headers: { "Cache-Control": "no-store" } }
     );
   } catch (error) {
     console.error("Error in /api/courses:", error);
 
     return NextResponse.json(
       { success: false, message: "Failed to fetch courses" },
-      { status: 500 }
+      { status: 500 ,  headers: { "Cache-Control": "no-store" }  }
     );
   }
 }
--- helper/db.js
+++ helper/db.js
@@ -1,18 +1,52 @@
-import mongoose from "mongoose"
-// import { User } from "../models/user"
+import mongoose from "mongoose";
 
-export const connectDb = async() => {
-    if (mongoose.connection.readyState >= 1) return;
-    try {
-         await mongoose.connect(process.env.MONGO_DB_URL,{
-            dbName:'course_manager'
-        })
-        // console.log("DB connected ...",connection);
-        // console.log("user is created");
-        
-    } catch (error) {
-        console.log("failed to connect with database");
-        console.log(error);
-        
-    }
+const MONGODB_URI = process.env.MONGO_DB_URL;
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGO_DB_URL in your .env file");
 }
+
+let cached = (global).mongoose;
+if (!cached) {
+  cached = (global).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDb() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "course_manager",
+      bufferCommands: false,
+    }).then((mongoose) => mongoose);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (err) {
+    cached.promise = null; // reset so future calls retry
+    throw err;
+  }
+
+  return cached.conn;
+}
+
+
+
+// import mongoose from "mongoose"
+// // import { User } from "../models/user"
+
+// export const connectDb = async() => {
+//     if (mongoose.connection.readyState >= 1) return;
+//     try {
+//          await mongoose.connect(process.env.MONGO_DB_URL,{
+//             dbName:'course_manager'
+//         })
+//         // console.log("DB connected ...",connection);
+//         // console.log("user is created");
+        
+//     } catch (error) {
+//         console.log("failed to connect with database");
+//         console.log(error);
+        
+//     }
+// }
