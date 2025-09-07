88a18e715af18d0f7bda80383d48cda045728137
Aaliya3032 <khan.alia3032@gmail.com>
2025-09-07 16:48:36 +0530

    login session delete after 1 hour

--- auth.config.js
+++ auth.config.js
@@ -1,6 +1,11 @@
 export const authConfig = {
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt',
+        maxAge: 60 * 60,
+        // updateAge: 0,    // force fixed expiry
     },
+     jwt: {
+    maxAge: 60 * 60, // 1 hour
+  },
     providers: [],
 }
