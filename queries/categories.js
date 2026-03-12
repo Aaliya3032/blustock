69b7a6c6c84974693943f55b05dc8259cef8eb6e
Mohammad Raja <mohammadraja5436@gmail.com>
2026-03-13 01:44:41 +0530

    Commit

--- queries/categories.js
+++ queries/categories.js
@@ -1,17 +1,26 @@
 import { replaceMongoIdInArray, replaceMongoIdInObject } from "@/lib/convertData";
 import { Category } from "@/models/category";
-
+import connectDB from "@/lib/db";   // ✅ add this
 
 export async function getCategories(){
+
+    await connectDB();   // ✅ connect first
+    
     const categories = await Category.find({}).lean();
     
     return replaceMongoIdInArray(categories);
 }
 
 export async function getCategoryDetails(categoryId){
+
     try {
+
+        await connectDB();   // ✅ connect first
+
         const category = await Category.findById(categoryId).lean();
+
         return replaceMongoIdInObject(category);
+
     } catch (error) {
         throw new Error(error);
     }
